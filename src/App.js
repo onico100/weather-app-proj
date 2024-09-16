ebb2c10d5e6823f1926729fe84f690ee5091594d
onico100 <142163577+onico100@users.noreply.github.com>
2024-09-16 18:16:39 +0300

    loading real data

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import WeatherSections from "./components/WeatherSections";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  const places = [
-    {
-      name: "Place 1",
-      description: "Sunny",
-      temperature: 30,
-      feelsLike: 32,
-      humidity: 45,
-    },
-    {
-      name: "Place 2",
-      description: "Cloudy",
-      temperature: 22,
-      feelsLike: 24,
-      humidity: 60,
-    },
-    {
-      name: "Place 3",
-      description: "Rainy",
-      temperature: 18,
-      feelsLike: 18,
-      humidity: 85,
-    },
-    {
-      name: "Place 4",
-      description: "Windy",
-      temperature: 25,
-      feelsLike: 23,
-      humidity: 50,
-    },
+  const [weatherData, setWeatherData] = useState([]);
+  const apiKey = "8ee633956bad6ae1965b557a94ecfcba";
+
+  const cities = [
+    { name: "New York", id: "5128581" },
+    { name: "London", id: "2643743" },
+    { name: "Alaska", id: "5879092" },
+    { name: "Eilat", id: "295277" },
   ];
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const weatherResults = await Promise.all(
+        cities.map(async (city) => {
+          const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?id=${city.id}&units=metric&appid=${apiKey}`
+          );
+          return {
+            name: city.name,
+            description: response.data.weather[0].description,
+            temperature: response.data.main.temp,
+            feelsLike: response.data.main.feels_like,
+            humidity: response.data.main.humidity,
+          };
+        })
+      );
+      setWeatherData(weatherResults);
+    };
+
+    fetchWeatherData();
+  }, []);
 
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <WeatherSections places={places} />
+      {weatherData.length > 0 ? (
+        <WeatherSections places={weatherData} />
+      ) : (
+        <p>Loading weather data...</p>
+      )}
     </div>
   );
 }
